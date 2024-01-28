38de07edcbecf0ee8b8b494a64438657ee2b2d55
clarnx <clarenceonline@outlook.com>
2024-01-28 14:44:50 +0000

    added current time function

---
+++ netlify/functions/current-time/src/main.rs
@@ -0,0 +1,48 @@
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use chrono::Utc;
+use http::header::HeaderMap;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use serde::Serialize;
+use serde_json::Value;
+use simple_logger::SimpleLogger;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+#[derive(Debug, Serialize)]
+struct ResponseData {
+    current_time: String,
+}
+
+pub(crate) async fn my_handler(
+    _event: ApiGatewayProxyRequest,
+    _ctx: Context,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    let current_time = Utc::now().to_rfc2822();
+
+    let current_time_data = serde_json::to_string(&ResponseData {
+        current_time: current_time,
+    })
+    .unwrap();
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: HeaderMap::new(),
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Text(current_time_data)),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
+}
