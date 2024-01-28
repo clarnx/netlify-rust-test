a9188925f399a2a6d7b41c255adf3e1b79cd3e6e
clarnx <clarenceonline@outlook.com>
2024-01-28 21:26:22 +0000

    fix bug

--- netlify/functions/hello/src/main.rs
+++ netlify/functions/hello/src/main.rs
@@ -1,5 +1,6 @@
 use aws_lambda_events::encodings::Body;
 use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use aws_lambda_events::lambda_function_urls::LambdaFunctionUrlRequest;
 use http::header::HeaderMap;
 use lambda_runtime::{service_fn, Error, LambdaEvent};
 use log::LevelFilter;
@@ -21,19 +22,21 @@ async fn main() -> Result<(), Error> {
 }
 
 pub(crate) async fn my_handler(
-    event: LambdaEvent<Value>,
+    event: LambdaEvent<LambdaFunctionUrlRequest>,
 ) -> Result<ApiGatewayProxyResponse, Error> {
     // let payload = serde_json::from_value(event.payload);
- 
-    let payload = event.payload;
 
-    let message = format!("Hello world from {}", payload.get("path").unwrap());
+    let payload: LambdaFunctionUrlRequest = event.payload;
+
+    let path = payload.raw_path.unwrap();
+
+    // let message = format!("Hello world from {}", payload.get("path").unwrap());
 
     let resp = ApiGatewayProxyResponse {
         status_code: 200,
         headers: HeaderMap::new(),
         multi_value_headers: HeaderMap::new(),
-        body: Some(Body::Text(serde_json::to_string(&payload).unwrap())),
+        body: Some(Body::Text(path)),
         is_base64_encoded: false,
     };
 
