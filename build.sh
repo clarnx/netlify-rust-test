37fdcebc98b26a3b7512a085f98eeadeec39826e
clarnx <clarenceonline@outlook.com>
2024-01-29 22:25:44 +0000

    updates

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-set -euxo pipefail
-
-cargo zigbuild --target x86_64-unknown-linux-gnu.2.26 --release
-mkdir -p functions
-cp target/x86_64-unknown-linux-gnu/release/hello /opt/build/repo/functions
-cp target/x86_64-unknown-linux-gnu/release/current-time /opt/build/repo/functions
-cp target/x86_64-unknown-linux-gnu/release/axum-lambda-demo /opt/build/repo/functions
