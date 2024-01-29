5845300899613d6c96e9987b79387c5b37a0197c
clarnx <clarenceonline@outlook.com>
2024-01-29 20:48:57 +0000

    lambda with axum

---
+++ netlify/functions/axum-lambda-demo/src/main.rs
@@ -0,0 +1,71 @@
+//! This is an example function that leverages the Lambda Rust runtime HTTP support
+//! and the [axum](https://docs.rs/axum/latest/axum/index.html) web framework.  The
+//! runtime HTTP support is backed by the [tower::Service](https://docs.rs/tower-service/0.3.2/tower_service/trait.Service.html)
+//! trait.  Axum's applications are also backed by the `tower::Service` trait.  That means
+//! that it is fairly easy to build an Axum application and pass the resulting `Service`
+//! implementation to the Lambda runtime to run as a Lambda function.  By using Axum instead
+//! of a basic `tower::Service` you get web framework niceties like routing, request component
+//! extraction, validation, etc.
+use axum::http::StatusCode;
+use axum::{
+    extract::Path,
+    response::Json,
+    routing::{get, post},
+    Router,
+};
+use lambda_http::{run, Error};
+use serde_json::{json, Value};
+use std::env::set_var;
+
+async fn root() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /" }))
+}
+
+async fn get_foo() -> Json<Value> {
+    Json(json!({ "msg": "I am GET /foo" }))
+}
+
+async fn post_foo() -> Json<Value> {
+    Json(json!({ "msg": "I am POST /foo" }))
+}
+
+async fn post_foo_name(Path(name): Path<String>) -> Json<Value> {
+    Json(json!({ "msg": format!("I am POST /foo/:name, name={name}") }))
+}
+
+/// Example on how to return status codes and data from an Axum function
+async fn health_check() -> (StatusCode, String) {
+    let health = true;
+    match health {
+        true => (StatusCode::OK, "Healthy!".to_string()),
+        false => (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Not healthy!".to_string(),
+        ),
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    // AWS Runtime can ignore Stage Name passed from json event
+    // Remove if you want the first section of the url to be the stage name of the API Gateway
+    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
+    set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
+
+    // required to enable CloudWatch error logging by the runtime
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/foo", get(get_foo).post(post_foo))
+        .route("/foo/:name", post(post_foo_name))
+        .route("/health/", get(health_check));
+
+    run(app).await
+}
